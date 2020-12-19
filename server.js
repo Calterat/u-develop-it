98efc40914d8eb981ee48c6df662f11b6c84493e
Calterat <Calterat@gmail.com>
2020-12-18 18:52:36 -0600

    added db connection and query. Also Nodemon

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,17 +8,30 @@ const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+  if (err) return console.error(err.message);
+  console.log('Connected to the election database.');
+});
+
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hello World'
+    message: 'Hello World!'
   });
 });
+
+db.all(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+})
 
 // Default response for any other request(Not Found) Catch all request
 app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Start server after DB connection is
+db.on('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
